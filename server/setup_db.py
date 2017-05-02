481c75cc95580e79451db6f451c7137c83ddb679
Celeo <celeodor@gmail.com>
2017-05-01 20:06:33 -0700

    Working

--- server/setup_db.py
+++ server/setup_db.py
@@ -1,7 +1,27 @@
 #!/usr/bin/env python
+from datetime import datetime
+import time
+import sys
+
 from app.app import db
-from app.models import *
 
 
-db.create_all()
-print('Database models created')
+def log(s):
+    print(f'[{datetime.now()}] {s}')
+
+
+def main():
+    for i in range(5):
+        try:
+            print(f'Attempt #{i} to connect to database ...')
+            db.create_all()
+            print('Database models created')
+            break
+        except Exception as e:
+            log(f'Database not ready: {e}')
+            time.sleep(5)
+        sys.stdout.flush()
+
+
+if __name__ == '__main__':
+    main()
