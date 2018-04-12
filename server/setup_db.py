5ef6e3a3f1657cfec1bc8aff0fdfeb2a73abed99
Celeo <celeodor@gmail.com>
2018-04-12 16:20:17 -0700

    Un-docker-fying server code

--- server/setup_db.py
+++ server/setup_db.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 from datetime import datetime
-import time
-import sys
 
 from app.app import db
 
@@ -11,16 +9,8 @@ def log(s):
 
 
 def main():
-    for i in range(5):
-        try:
-            print(f'Attempt #{i+1} to connect to database ...')
-            db.create_all()
-            print('Database models created')
-            break
-        except Exception as e:
-            log(f'Database not ready: {e}')
-            time.sleep(5)
-        sys.stdout.flush()
+    log(f'Attemping to create database ...')
+    db.create_all()
 
 
 if __name__ == '__main__':
